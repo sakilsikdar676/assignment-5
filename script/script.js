cbdecc06bbc4457c1258f776da19d074ec1a3d80
MASRUL ISLAM <sakilsikdar676@gmail.com>
2026-03-07 05:55:20 +0600

    login page complited

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const goHome = () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    if (username === "admin" && password === "admin123") {
+        window.location.href = "./home.html";
+    }else{
+        alert("Invalid details");
+    }
+}
